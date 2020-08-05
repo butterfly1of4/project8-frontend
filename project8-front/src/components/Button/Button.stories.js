97a3f32900011d3707298d7a457cde3efde6d3e0
Michael Hall <mhalla40@gmail.com>
2020-08-05 11:48:40 -0400

    added Button component with search button

---
+++ project8-front/src/components/Button/Button.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Button from "./Button";
+
+function onClick() {
+  alert("you clicked me");
+}
+
+storiesOf("Button", module).add("Search", () => (
+  <Button type="search" whenClicked={onClick} label="Search" />
+));
