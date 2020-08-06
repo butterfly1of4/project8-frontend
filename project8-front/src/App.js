ab4b7199f6c75167768ff3063a5ac562dbc465d2
Alex_Kelly <afkelly14@Gmail.com>
2020-08-05 20:37:59 -0400

    added background image on homepage

--- project8-front/src/App.js
+++ project8-front/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import logo from "../src/logo.png";
-
+import logo from "./images/logo.png";
+import backgroundimage from "./images/backgroundimage.jpg";
 import { Route, Link, Switch } from "react-router-dom";
 import "./App.css";
 import newReleases from "./components/newReleases";
@@ -47,15 +47,20 @@ class App extends Component {
     return (
       <div>
         <nav>
-          <img className="logo" src={logo} />
+          <Link to="/">
+            <img className="logo" src={logo} />
+          </Link>
         </nav>
         <main>
-          <Route path="/newReleases" exact component={newReleases} />
-          <Route path="/expiring" exact component={expiring} />
-          <Route
-            path="/newReleases/update/:title"
-            render={(routerProps) => <Update {...routerProps}></Update>}
-          />
+          <Switch>
+            <Route path="/newReleases" exact component={newReleases} />
+            <Route path="/expiring" exact component={expiring} />
+            <Route
+              path="/newReleases/update/:title"
+              render={(routerProps) => <Update {...routerProps}></Update>}
+            />
+          </Switch>
+          <img className="curtain" src={backgroundimage}/>
         </main>
       </div>
     );
