7735008d796f274e1e3c0124e74def32894535e6
Alex_Kelly <afkelly14@Gmail.com>
2020-08-06 15:12:59 -0400

    home page buttons complete

--- project8-front/src/Home.js
+++ project8-front/src/Home.js
@@ -5,22 +5,23 @@ import backgroundimage from "./images/backgroundimage.jpg";
 import "./App.css";
 
 class Home extends Component {
-    render () {
-        return (
-            <>
-            <img className="curtain" src={backgroundimage} />
-            <Switch>
-            <Link to="/expiring">
-            <button className="HPLinkButton">Expiring</button>
+  render() {
+    return (
+      <>
+        <img className="curtain" src={backgroundimage} />
+        <Switch>
+          <Link to="/expiring">
+            <button className="HPLinkButtonE">Expiring</button>
           </Link>
-         
+        </Switch>
+        <Switch>
           <Link to="/newReleases">
-            <button className="HPLinkButton">New Releases</button>
+            <button className="HPLinkButtonNR">New Releases</button>
           </Link>
-          </Switch>
-          </>
-        )
-    }
+        </Switch>
+      </>
+    );
+  }
 }
 
 export default Home;
