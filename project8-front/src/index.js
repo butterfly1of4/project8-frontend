bbd907aeb796a03a15081c09666964a5ca226cbf
butterfly1of4 <jhouck29@gmail.com>
2020-08-07 14:06:34 -0400

    redeploy gitpages

--- project8-front/src/index.js
+++ project8-front/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import "@testing-library/jest-dom/extend-expect";
+// np
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
